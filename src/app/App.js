cdd254e1bf66c304267cf0dbd9457df7611b63f4
Monique de Haard <moniquedehaard@msn.com>
2021-09-26 16:42:59 +0200

    test

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> Hello welcome </header>
+      <p> This is my page </p>
+      <p> Test </p>
+    </div>
+  );
+}
+
+export default App;
